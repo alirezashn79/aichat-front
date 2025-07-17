8b3da6875178d4b7f8e0be48c4517ce17314d62d
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2025-07-17 10:59:44 +0330

    fix clerk

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
